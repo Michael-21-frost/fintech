ab2a628770bd60c93227af527543abd59d84bb8e
Michael otote <michaelotote9@gmail.com>
2023-07-11 08:09:11 +0100

    Add files via upload

---
+++ public/script/menu.js
@@ -0,0 +1,6 @@
+function toggleMenu(){
+    let sidebar = document.querySelector('.sidebar');
+    let toggle = document.querySelector('.link');
+    sidebar.classList.toggle('active');
+    toggle.classList.toggle('active');
+}
